359b62b41e47db56cb0b8169787b9c41db0625b6
Meghthedev <meghthedev@gmail.com>
2023-05-03 22:35:00 +0530

    script: add snippet to do basic rom bringup

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 echo Script to clone trees for m52xq :D
+echo "Is your rom LineageOS based? (y or n)"
+read losornot
+
+if [ "$losornot" = "n" ]; then
+echo "Please tell the name of your rom (used for bringup)"
+read romname
+fi
 
 git clone https://github.com/paulowesll/android_device_samsung_m52xq device/samsung/m52xq 
 echo dt done !
@@ -16,9 +23,17 @@ echo kernel done!
 
 if [ ! -d "hardware/samsung" ]; then
 git clone https://github.com/LineageOS/android_hardware_samsung hardware/samsung 
-echo hw/samsung done!
+echo hw/samsung done !
 fi
 
 if [ ! -d "prebuilts/clang/host/linux-x86/clang-r416183b1" ]; then
 git clone https://github.com/ArrowOS-Devices/android_prebuilts_clang_host_linux-x86_clang-r416183b1 prebuilts/clang/host/linux-x86/clang-r416183b1 
+echo clang cloned !
 fi
+
+if [ "$losornot" = "n" ]; then
+cd device/samsung/m52xq
+sed -i 's/lineage/$romname/g' AndroidProducts.mk
+sed -i 's/lineage/$romname/g' lineage_m52xq.mk
+fi
+
